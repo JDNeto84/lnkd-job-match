8ab5acd847c664ee36fb1cae989c5797525eab2f
João Neto <j040n3t0@gmail.com>
2026-02-14 20:53:23 -0300

    first commit

---
+++ matchjob/src/main/java/com/lnkd/matchjob/MatchjobApplication.java
@@ -0,0 +1,13 @@
+package com.lnkd.matchjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchjobApplication.class, args);
+	}
+
+}
